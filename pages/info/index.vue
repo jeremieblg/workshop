bd6bb56b7e70eb97f359b83249c87e3fa8fd0ce5
Romain <rbohuon@gmail.com>
2020-04-06 17:24:20 +0200

    question salon OK v2

--- pages/info/index.vue
+++ pages/info/index.vue
@@ -1,7 +1,27 @@
 <template>
   <div class="container">
     <navbar></navbar>
-    <div>info</div>
+    <div class="main-div">
+      <table class="table-sum">
+        <tr>
+          <td>
+            <table class="activity-table">
+              <tr>
+                <td @click="goTo('sdb')">
+                  Activit&eacute; n°1 : Salle de bain
+                </td>
+              </tr>
+            </table>
+
+            <table class="activity-table">
+              <tr>
+                <td @click="goTo('salon')">Activit&eacute; n°2 : Salon</td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -12,11 +32,41 @@ export default {
   components: {
     navbar
   },
-  data() {
-    return {}
-  },
-  methods: {}
+
+  methods: {
+    goTo(param) {
+      if (param === 'sdb') {
+        this.$router.push({
+          path: '/activity/1'
+        })
+      } else if (param === 'salon') {
+        this.$router.push({
+          path: '/activity/2'
+        })
+      }
+    }
+  }
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.container {
+  flex-direction: column;
+  justify-content: center;
+}
+
+table.activity-table {
+  width: 18em;
+  height: 10em;
+  margin: 2em;
+  td {
+    background-color: $color-light-green;
+    font-size: 30px;
+    color: $color-grey;
+    &:hover {
+      background-color: $color-dark-green;
+      cursor: pointer;
+    }
+  }
+}
+</style>
