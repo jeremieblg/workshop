7c0a83ef9c1f59c5ed46f15acdb7130baa0b3093
Romain <rbohuon@gmail.com>
2020-04-06 16:15:58 +0200

    question salon OK

--- pages/activity/2/index.vue
+++ pages/activity/2/index.vue
@@ -5,16 +5,8 @@
       :current-question="currentQuestion"
       :nb-max-question="nbMaxQuestion"
     ></progressbar>
-    <div class="activityContainer">
-      <div class="mainTitle">Salon</div>
-      <div class="mainCorp">
-        Une multi prise est allumée sur laquelle sont branché la box internet,
-        TV et console de jeux. <br />
-        Vous avez me choix d'éteindre la multiprise.
-      </div>
-      <el-swicth> </el-swicth>
-    </div>
-    <div class="main-button">
+    <question :activity="currentActivity" :question="curQuestion"></question>
+    <div class="main-button" @click="submit()">
       <span>Suivant</span>
     </div>
   </div>
@@ -23,19 +15,37 @@
 <script>
 import navbar from '~/components/navbar.vue'
 import progressbar from '~/components/progressBar.vue'
+import question from '~/components/question.vue'
 
 export default {
   components: {
     navbar,
-    progressbar
+    progressbar,
+    question
   },
   data() {
     return {
       currentQuestion: 1,
-      nbMaxQuestion: 2
+      nbMaxQuestion: 2,
+      currentActivity: 'salon'
     }
   },
-  methods: {}
+  computed: {
+    curQuestion() {
+      return this.currentQuestion - 1
+    }
+  },
+  methods: {
+    submit() {
+      if (this.currentQuestion === 2) {
+        this.$router.push({
+          path: '/info'
+        })
+      } else {
+        this.currentQuestion += 1
+      }
+    }
+  }
 }
 </script>
 
@@ -44,49 +54,7 @@ export default {
   flex-direction: column;
   justify-content: flex-start;
 }
-.activityContainer {
-  background-color: $color-black;
-  height: 500px;
-  width: 100%;
-  border-bottom-left-radius: 30px;
-  border-bottom-right-radius: 30px;
-  z-index: 2;
-  padding-top: 40px;
-  padding-bottom: 70px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  padding-left: 32px;
-  padding-right: 32px;
-  align-items: center;
-  .mainTitle {
-    font-size: 25px;
-    color: $color-light-green;
-  }
-  .mainCorp {
-    text-align: left;
-    color: $color-light-green;
-  }
-  .mainInfo {
-    text-align: left;
-  }
-  .mainForm {
-    height: 100px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    .containerInput {
-      display: flex;
-      justify-content: space-between;
-      .name {
-        width: 70%;
-      }
-      .age {
-        width: 20%;
-      }
-    }
-  }
-}
+
 .main-button {
   width: 100%;
   height: 70px;
